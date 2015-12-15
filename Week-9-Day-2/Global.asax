bef9381a2b6f518872659e967b44fd432afb9ddf
Cason Barnhill <casonbarnhill@yahoo.com>
2015-12-15 14:44:54 -0600

    Initial Create and Modify Application User

---
+++ Week-9-Day-2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week_9_Day_2.MvcApplication" Language="C#" %>
---
+++ Week-9-Day-2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week_9_Day_2.Startup))]
+namespace Week_9_Day_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
