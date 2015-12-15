262d03cffeeca24e4a014d87bbfb92b39a18a116
Cason Barnhill <casonbarnhill@yahoo.com>
2015-12-15 17:29:12 -0600

    modified index

--- Week-9-Day-2/Controllers/HomeController.cs
+++ Week-9-Day-2/Controllers/HomeController.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Week_9_Day_2.Models;
 
 namespace Week_9_Day_2.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            //  db.GetUser()
+           var UserId= User.Identity.GetUserId();
+            var db = new ApplicationDbContext();
+            var user= db.Users.Find(UserId);
+            
+            return View(user);
         }
 
         public ActionResult About()
